7afa7e526224fd4f1aee1eb55bf5b14e27b4d526
mehdinemat <nematnejad.mm@gmail.com>
2023-03-16 08:55:15 -0700

    update auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 const auth =async (req,res , next)=>{
 
     try{
+        console.log(req.header)
         const token = req.header('Authorization')
         console.log(token , 'this is token authen')
         if(!token){
