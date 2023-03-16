657492163514e86097046414d72c2d6434e79fce
mehdinemat <nematnejad.mm@gmail.com>
2023-03-16 02:51:18 -0700

    new cookie change

--- controllers/authController.js
+++ controllers/authController.js
@@ -73,6 +73,7 @@ const userAuth = {
             path: '/api/refresh_token',
             maxAge: 30*24*60*60*1000
         })
+        res.setHeader('Set-Cookie', 'isLoggedin=true');
         console.log(req.cookies)
       
        return res.json({msg:'login success' , accessToken , user:{
