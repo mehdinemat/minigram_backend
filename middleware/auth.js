a8e379056044ced69af3e06d0ec503e1c355c31e
mehdinemat <nematnejad.mm@gmail.com>
2023-03-16 11:26:53 -0700

    update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,17 +4,14 @@ const auth =async (req,res , next)=>{
 
     try{
         const token = req.header('Authorization')
-        console.log(token , 'token')
         if(!token){
             return res.status(400).json({error:"Invalid Authentication"})
         }
         const decode = jwt.verify(token , process.env.SECRET_CREATE_CODE)
-        console.log(decode.id , process.env.SECRET_CREATE_CODE , 'this is secret code')
         if(!decode) {
             return res.status(400).json({error:"Invalid Authentication"})
         }
         const user = await User.findOne({_id:decode.id})
-        console.log(user , 'user auth')
         req.user = user
         next()
 
